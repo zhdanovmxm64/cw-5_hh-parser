e558e39a3055b0399742876af24bc25d4264139e
zhdanovmxm64 <zhdanovmxm64@gmail.com>
2023-08-01 21:02:52 +0400

    magic method

--- src/db_manager.py
+++ src/db_manager.py
@@ -4,3 +4,13 @@ class DBManager:
     '''
     Класс для работы с данными базы данных
     '''
+
+    def __init__(self, database: DataBase) -> None:
+        self.database = database
+        self.__connection = database.connection
+
+    def __repr__(self) -> str:
+        return f'{self.__class__.__name__}({type(self.database)})'
+
+    def __str__(self) -> str:
+        return 'Менеджер для работы с данными БД'
