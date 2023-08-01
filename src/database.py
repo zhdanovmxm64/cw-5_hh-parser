a8a098f48d00ebfb0142c8f67f05c75329995ae4
zhdanovmxm64 <zhdanovmxm64@gmail.com>
2023-08-01 21:01:05 +0400

    new function's

--- src/database.py
+++ src/database.py
@@ -28,3 +28,95 @@ class DataBase:
 
     def __str__(self) -> str:
         return f'База данных {self.__db_name}'
+
+    def __create_db(self) -> None:
+        '''
+        Создаёт базу данных с переданным пользователем названием
+        '''
+        try:
+            # подключение к бд postgres
+            connection = psycopg2.connect(dbname='postgres', **self.__db_data)
+            connection.autocommit = True
+
+            with connection.cursor() as cursor:
+                cursor.execute(f'CREATE DATABASE {self.__db_name}')
+
+            connection.close()
+        except psycopg2.errors.DuplicateDatabase:
+            pass
+        finally:
+            connection.close()
+
+    def __create_tables(self) -> None:
+        '''
+        Заполняет созданнную бд таблицами, которые создаются в последствии
+        выполнения кода из файла queries.sql
+        '''
+        # подключение к созданной бд
+        connection = self.__connection
+        filepath = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'queries.sql')
+
+        with connection.cursor() as cursor:
+            # создание таблиц в базе данных
+            with open(filepath, 'r', encoding='UTF-8') as sql_file:
+                commands = sql_file.read()
+                cursor.execute(commands)
+
+        connection.commit()
+
+    def fill_data(self, employer_list: list) -> None:
+        '''
+        Заполняет созданные таблицы данными о работадателях
+        и их вакансиях
+        :param employer_list: список объектов класса Employer
+        '''
+        connection = self.__connection
+        with connection.cursor() as cursor:
+            for employer in employer_list:
+                cursor.execute(
+                    'INSERT INTO employers (name, type, area, description, employer_url, site_url, '
+                    'open_vacancies) VALUES (%s, %s, %s, %s, %s, %s, %s)',
+                    employer.get_employer_inf()
+                )
+
+                cursor.execute('SELECT COUNT(*) FROM employers')
+                last_employer = cursor.fetchone()[0]
+
+                # перебирает все вакансии работадателя
+                for vacancy in Vacancy.get_vacancies_inf(employer.vacancies_list):
+                    # добавляет в каждую вакансию employer_id
+                    vacancy.insert(1, last_employer)
+                    vacancy_inf = tuple(vacancy)
+
+                    cursor.execute(
+                        'INSERT INTO vacancies (name, employer_id, description, area, salary_from, '
+                        'salary_to, salary, currency, experience, employment, address, url) '
+                        'VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)',
+                        vacancy_inf
+                    )
+
+            connection.commit()
+
+    def close_connection(self):
+        '''
+        Закрывает соединение с базой данных
+        '''
+        connection = self.__connection
+        connection.close()
+
+    @property
+    def db_name(self):
+        '''
+        Возвращает имя базы данных
+        :return: имя базы данных
+        '''
+        return self.__db_name
+
+    @property
+    def connection(self):
+        '''
+        Возвращает строку подключения к созданой бд
+        (необходимо для менеджера баз данных)
+        :return: строку подключения
+        '''
+        return self.__connection
